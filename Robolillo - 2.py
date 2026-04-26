93564acd164fc40ba3adbfbfad44273076f690f8
Arbolillo <Arbolillogaymer@gmail.com>
2026-04-26 02:22:34 -0600

    Things?

--- Robolillo - 2.py
+++ Robolillo - 2.py
@@ -18,7 +18,7 @@ async def on_ready():
     print(f"✅ Robolillo conectado como {bot.user}")
     print(f"📡 Servidores conectados: {[g.name for g in bot.guilds]}")
     
-    cogs = cogs = ["cogs.music", "cogs.fun", "cogs.moderation", "cogs.download", "cogs.ayuda", "cogs.juegos"]
+    cogs = cogs = ["cogs.music", "cogs.fun", "cogs.moderation", "cogs.download", "cogs.menus", "cogs.games"]
     for cog in cogs:
         try:
             await bot.load_extension(cog)
